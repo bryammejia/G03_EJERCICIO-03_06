bcd8c76f019c33f88c7abaec689b4a6e21da33ed
USER <USER@DESKTOP-97I3UFU>
2022-06-30 23:19:09 -0500

    G03_EJERCICIO_03_06 Bryam Mejia Juan Coraisaca Jonathan Coronel Programa finalizada

---
+++ src/main/java/Figuras/Figura.java
@@ -0,0 +1,60 @@
+package Figuras;
+
+
+import java.awt.Color;
+import java.awt.Point;
+
+
+/**
+ *
+ * @author USER
+ */
+public class Figura {
+    
+    private String tipo;
+    private Color color;
+    private int alto;
+    private int ancho;
+    private Point punto;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getAlto() {
+        return alto;
+    }
+
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+    
+    public Point getPunto() {
+        return punto;
+    }
+
+    public void setPunto(Point punto) {
+        this.punto = punto;
+    }
+
+}
---
+++ src/main/java/Figuras/Principal.java
@@ -0,0 +1,16 @@
+package Figuras;
+
+
+/**
+ *
+ * @author USER
+ */
+public class Principal {
+
+    public static void main(String[] args)
+    {
+        Ventana1 v1=new Ventana1();
+        v1.setVisible(true);
+    }
+
+}
